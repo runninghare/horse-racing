bc2dac3966c10aef23316d92a16a578b552183dd
Ross Zhu <ross.yuexin.zhu@gmail.com>
2025-01-24 12:57:11 +1100

    Add tailwindcss.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
-import './tailwind.css'
 import SpinningWheel from './SpinningWheel.tsx'
 
 createRoot(document.getElementById('root')!).render(
