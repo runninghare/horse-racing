9f3c44b11b6d7e0d1529157d51bac9b16faafd61
Ross Zhu <ross.yuexin.zhu@gmail.com>
2025-04-15 17:52:35 +1000

    Add Kristian.

--- src/constants/participants.constant.ts
+++ src/constants/participants.constant.ts
@@ -4,5 +4,6 @@ export const PARTICIPANTS = [
     { name: 'Ross', present: true, selected: false, color: '#3B82F6' },
     { name: 'Amy', present: true, selected: false, color: '#F59E0B' },
     { name: 'Sumit', present: true, selected: false, color: '#10B981' },
-    { name: 'Elmer', present: true, selected: false, color: '#10B981' }
+    { name: 'Elmer', present: true, selected: false, color: '#F43F5E' },
+    { name: 'Kristian', present: true, selected: false, color: '#0EA5E9' },
 ]
